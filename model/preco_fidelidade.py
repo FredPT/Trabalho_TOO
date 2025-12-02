96b490b7d23259cf525fa0a58e30edfea6534a3b
FredPT <fred.taufer@gmail.com>
2025-12-01 22:32:01 -0300

    att Precos

--- model/preco_fidelidade.py
+++ model/preco_fidelidade.py
@@ -2,4 +2,4 @@ from model.price_strategy import PriceStrategy
 
 class PrecoFidelidade(PriceStrategy):
     def calcular(self, preco_base):
-        return preco_base * 0.9  # 10% desconto
+        return preco_base * 0.9
--- model/preco_promocional.py
+++ model/preco_promocional.py
@@ -2,4 +2,4 @@ from model.price_strategy import PriceStrategy
 
 class PrecoPromocional(PriceStrategy):
     def calcular(self, preco_base):
-        return preco_base * 0.8  # 20% desconto
+        return preco_base * 0.8
